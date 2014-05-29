44a1ebd4a705e101c9d782106f302cd14eaaeae1
Jorge Vargas <jorgevargaslarrota@hotmail.com>
2014-05-29 14:10:35 -0500

    Update socialsharing.php

--- socialsharing.php
+++ socialsharing.php
@@ -46,7 +46,7 @@ class SocialSharing extends Module
 
 		$this->displayName = $this->l('Social sharing');
 		$this->description = $this->l('Displays social sharing buttons (Twitter, Facebook, Google+ and Pinterest) on every product page.');
-	}hookDisplayHeader
+	}
 
 	public function install()
 	{
